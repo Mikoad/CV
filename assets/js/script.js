cc9679d728c074b090519a0692611dc7265ada5e
Adrien Miko <mikolajczykadrien@gmail.com>
2024-08-02 16:51:22 +0200

    make

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,9 +46,106 @@ const seeMore = document.querySelectorAll(".seemore");
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 120) {
+  if (window.scrollY > 125) {
     navbar.classList.add("nav-scrolled");
   } else {
     navbar.classList.remove("nav-scrolled");
   }
+});
+
+//form
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form-contact");
+  const inputs = document.querySelectorAll("input[type='text'], textarea");
+
+  let email, textarea;
+
+  const errorDisplay = (tag, message, valid) => {
+    const container = document.querySelector("." + tag + "-container");
+    const span = container.querySelector("span");
+
+    if (!valid) {
+      container.classList.add("error");
+      span.textContent = message;
+    } else {
+      container.classList.remove("error");
+      span.textContent = "";
+    }
+  };
+
+  const emailChecker = (value) => {
+    if (!value.match(/^[\w._-]+@[\w-]+\.[a-z]{2,5}$/i)) {
+      errorDisplay("email", "Le format du mail n'est pas correct.", false);
+      email = null;
+    } else {
+      errorDisplay("email", "", true);
+      email = value;
+    }
+  };
+
+  const messageChecker = (value) => {
+    const sanitizedValue = value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    if (sanitizedValue.trim() === "") {
+      errorDisplay(
+        "message",
+        "Le message ne doit pas être vide ou contenir uniquement des balises HTML.",
+        false
+      );
+      textarea = null;
+    } else {
+      errorDisplay("message", "", true);
+      textarea = sanitizedValue;
+    }
+  };
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      switch (e.target.id) {
+        case "email":
+          emailChecker(e.target.value);
+          break;
+        case "message":
+          messageChecker(e.target.value);
+          break;
+        default:
+          break;
+      }
+    });
+  });
+
+  form.addEventListener("submit", (e) => {
+    const submitButton = document.querySelector("input[type='submit']");
+    e.preventDefault();
+    if (email && textarea) {
+      const data = {
+        email,
+        textarea,
+      };
+      fetch("https://hook.eu2.make.com/fzhbzfcscem4ksj6n2anj6htmm4owffd", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+      submitButton.value = "Message envoyé";
+      submitButton.style.background = "green";
+      submitButton.style.scale = "1";
+      console.log(data);
+      inputs.forEach((input) => {
+        input.value = "";
+      });
+      email = null;
+      textarea = null;
+    }
+  });
 });
